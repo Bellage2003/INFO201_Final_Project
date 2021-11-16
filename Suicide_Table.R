4471f3d0a523152ef689c34933a275cf508aa6fd
Bellage <gehuijun@uw.edu>
2021-11-15 18:54:45 -0800

    new change made to rmd file

--- Suicide_Table.R
+++ Suicide_Table.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(dplyr)
 
 suicide_rate <- read.csv("https://raw.githubusercontent.com/Bellage2003/INFO201_Final_Project/main/Data/Suicide%20Rates%20Overview.csv")
-View(suicide_rate)
+
 
 #Extract two countries(United State & Japan) our group want to compare
 suicide_table <- data.frame(
@@ -11,7 +11,6 @@ suicide_table <- data.frame(
     filter(country %in% c("United States", "Japan"))
     
 )
-View(suicide_table)
 
 #Extract the valuable information
 suicide_table <- subset(suicide_table, select = -c(country.year : generation))
@@ -25,3 +24,7 @@ suicide_table <- suicide_table %>%
   arrange(year)
 
 #All the data have already rounded so we don't need to change it.
+
+View(suicide_table)
+
+
