cf5596afd197129c3d8a46d93d28663847aaf379
Bellage <gehuijun@uw.edu>
2021-11-16 16:55:26 -0800

    new chart file made on Nov 16

---
+++ Suicide_Col_Chart.R
@@ -0,0 +1,30 @@
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+
+suicide_rate <- read.csv("https://raw.githubusercontent.com/Bellage2003/INFO201_Final_Project/main/Data/Suicide%20Rates%20Overview.csv")
+
+suicide_table <- data.frame(
+  suicide_rate %>% 
+    filter(country %in% c("United States", "Japan")) %>% 
+    filter(year == "2015") %>% 
+    mutate(age = word(age, 1, -2)) 
+  
+)
+
+
+suicide_table <- subset(suicide_table, select = -c(country.year : generation))
+
+#Because we want to compare the numbers in different age groups in two countird, so I use 
+#geom_col to make the chart.
+
+col_chart <- ggplot(suicide_table, aes(x = age , y = suicides_no, fill = country)) +
+  geom_col() + labs(title = "The Comparison of Suicide Numbers in Different Age Groups",
+                                                x = "Age Groups of People", 
+                                                y = "Numbers of Suicides"
+                                              )  +
+  theme(plot.title = element_text(size=9)) +
+  scale_fill_discrete(name = "Country's name")
+
+
+
