2acd3fb69a5d74de5e603523de0fc46bea414633
Tiffany Shields <trs31@uw.edu>
2021-12-07 20:36:49 -0800

    final changes

--- Final web/app.r
+++ Final web/app.r
@@ -40,9 +40,9 @@ intro_page <- tabPanel(
   "Introduction",
   h1("A Comparison Between the U.S. and Japan Suicide Rates"),
   p("Our group has been working with a data set that involved mental health in a variety of countries. We have decided to specifically focus on the comparison between the U.S. and Japan.
-     In the first chart, the information being shown is a comparison of suicide rates in men ages 25-34 in the U.S. and Japan. The shiny widget that will be used to portray this information is radio buttons.
-     In the second chart, the information being shown is a comparison of suicide rates between different genders. The information will be shown through a stacked bar chart and the widget being used for this is select box.
-     Lastly, the third chart will show a comparison between indifferent age groups between the U.S. and Japan. The widget that is used to portray this information is select boxes."),
+     In the first chart, the information being shown is a comparison of suicide rates in men ages 25-34 in the U.S. and Japan. The shiny widget that will be used to portray this information is radio buttons and select box.
+     In the second chart, the information being shown is a comparison of suicide numbers between different age groups. The information will be shown through a stacked bar chart and the widget being used for this is select box, and check box.
+     Lastly, the third chart will show a comparison of number of suicides between males and females in the U.S. and Japan. The widget that is used to portray this information is select boxes."),
   HTML('<center><img src="https://fodmap-publicsite-us-east-2.s3.amazonaws.com/production/media/images/Mental_Health__IBS__Effectiveness_of_psychologi.original.png" width="400"></center>')
   ,
 
@@ -50,8 +50,8 @@ intro_page <- tabPanel(
   h3("Exploration with the Data"),
   p("We wanted to explore the data set and what it had to offer in terms of comparison between the U.S. and Japan.
      Both of these countries are known to have competitive societies in different aspects, and we wanted to explore the mental health
-     of similarly aged inviduals in both of these countries as well as different age groups above and below. We also wanted to observe the differences
-     in mental health within different genders in both countries. Some general questions we wanted answered are: How does the U.S. and Japan suicide rates compare overall? Does the U.S. have higher amounts of suicides? What can we tell from the data points
+     of a variety of ages ranging from 15, to 75+. Additionally, we wanted to observe the differences
+     in mental health within different genders in both countries. Some general questions we want answered are: How does the number of U.S. and Japan suicides compare overall? Does the U.S. have higher amounts of suicides? What can we tell from the data points
     when comparing maximums and minimums for specific years, and what can we infer about what was occuring during these time periods?"),
   
   h4("Info 201, Group Names"),
@@ -156,15 +156,15 @@ conclusion_page <- tabPanel(
     the year 2005. The difference between U.S. and Japan was 1,204. We are able to gather from this data set that in 2005, Japan had a peak in suicides where the U.S. had lower numbers than years before."),
   p("In chart 2, we are able to see that in 2015, the lowest occurences of suicides happened in the age range of 5-14 years for both male and female. For Japan, both male and female had similar amounts of suicides for the age ranges
     of 35-54, and 55-74 whereas in the U.S., we are able to see that the age range of 35-54 had a higher number of suicides than age range of 55-74 in 2015."),
-  p("Lastly, in chart 3 we are able to directly compare the rates of the U.S. and Japan directly for male and female suicides. In 2015,
+  p("Lastly, in chart 3 we are able to directly compare the numbers of the U.S. and Japan directly for male and female suicides. In 2015,
     the charts show similarities between the two countries in that males in both countries have higher suicide numbers than females. Japan had lower overall suicides for 2015 numerically but when comparing the overall population for the
     two countries, it can show that Japan has a higher percentage of suicides for total population."),
   h3("Conclusion"),
   p("Overall, we are able to see that when comparing the U.S. and Japan directly, the U.S. has a higher amount of suicides
-    when considering the data numerically. It is consistent that men have higher suicide rates in both countries than the data collected for females. With the charts
-    listed in previous pages, we are able to see that the number of suicides seem to have an inverse relationship whereas one country's suicides decrease, the others increase. Due to the limitations of our data,
-    we are unable to pull any concrete conclusions as to why these numbers inverse other than inferring that events took place during those years that may have affected
-    the U.S. and Japan differently, which could affect the population's mental health and suicide rates."),
+    when considering the data numerically. It is consistent that men have higher number of suicides in both countries than the data collected for females. With the charts
+    listed in previous pages, we are able to see that the number of suicides seem to have an inverse relationship where one country's suicides decrease, the other's increase. Due to the limitations of our data,
+    we are unable to pull any concrete conclusions as to why these numbers inverse other than inferring that there may have been events that took place during those years that may have affected
+    the U.S. and Japan differently, which could affect the population's mental health and number of suicides."),
  
 )
 
